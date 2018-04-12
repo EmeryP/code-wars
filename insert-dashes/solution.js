5f49d806649dd2f97722583931b94359a8b74ff7
EmeryP <emeryparks09@hotmail.com>
2018-04-11 19:05:40 -0700

    files scaffolded

---
+++ insert-dashes/solution.js
@@ -0,0 +1,26 @@
+
+
+let num = '454793';
+
+function insertDash(num) {
+  
+  let numSplit = num.split('');
+  console.log(numSplit);
+  
+  // for(let i = 0; i < numSplit.length; i++){
+  
+    // if((numSplit[i] === ){ 
+      // push "" after i
+      // 
+  //       } else if(array1[j] !== array2[j] && array2[j] !== ""){
+  //         count -= 1;
+  //       } else if(array2[j] === " "){
+  //       count += 0;
+  //       }
+    // } 
+    $( "li" ).filter( ":even" ).css( "background-color", "red" );
+  
+}
+
+insertDash(num);
+
